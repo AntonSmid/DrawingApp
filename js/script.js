354d961cb753de8b8f540cdbda54d999353ec60a
AntonSmid <anton.smid@triera.net>
2017-08-06 00:53:15 +0200

    some code done

--- js/script.js
+++ js/script.js
@@ -10,16 +10,31 @@ $(function(){
     
 // declare variables
   // painting-erasing or not
+  var paint = false;    
   // painting or erasing
+  var paint_erase = "paint";    
   // get the canvas and context
+  var canvas = document.getElementById("paint");  
+  var ctx = canvas.getContext("2d");
+  
   // get the canvas container
+  var container = $("#container");
+  
   // mouse position
+  var mouse = {x:0, y:0};
   
 // onload load saved work from localStorage
     
 // set drawing parameters (lineWidth, lineJoin, lineCap)
+  ctx.lineWidth = 3;
+  ctx.lineJoin = "round";
+  ctx.lineCap = "round";
   
 // click inside container
+  container.mousedown(function(){
+    paint = true;
+    
+  });
   
 // move the mouse while holding mouse key
   
